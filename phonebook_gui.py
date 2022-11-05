bafb7c4dbb64ed119ab94aa390fa84a130818359
KatarinaMandic <katarina.mandic.97@gmail.com>
2022-11-05 20:23:55 +0100

    Added phonebook_gui.py

---
+++ phonebook_gui.py
@@ -0,0 +1,9 @@
+# Korišćenje PySimpleGUI biblioteke kreirati aplikaciju za telefonski
+# imenik. Interfejs ima deo za unos kontakta (ime, prezime, broj telefona,
+# mail) i deo za prikaz unetih kontakata u imenik.
+# • Svaki novi unos treba da bude kreiran kao objekat klase Kontakt. Klasu
+# proizvoljno kreirajte da poseduje potrebne podatke i metode.
+# • Svaki kreirani kontakt prikazuje se posle kreiranja u delu za prikaz
+# kontakata.
+# • Svi kontakti se snimaju u serijalizovanoj datoteci imenik.ser.
+# • Pri pokretanju aplikacije učitavaju se prethodno snimljeni kontakti.
