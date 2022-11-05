492e661d33ad3ef1d7059699c517f500448cda1d
KatarinaMandic <katarina.mandic.97@gmail.com>
2022-11-05 21:34:14 +0100

    Modified phonebook_gui (layout)

--- phonebook_gui.py
+++ phonebook_gui.py
@@ -7,3 +7,37 @@
 # kontakata.
 # • Svi kontakti se snimaju u serijalizovanoj datoteci imenik.ser.
 # • Pri pokretanju aplikacije učitavaju se prethodno snimljeni kontakti.
+
+import PySimpleGUI as sg
+
+sg.theme('DarkBlue')
+
+layout1 = [
+    [sg.Listbox('', size = (50,20))]
+]
+
+layout2 = [
+    [sg.Text('Create new User', justification='c', font=('Arial', 24))],
+    [sg.Text('')],
+    [sg.Text('First name: ', size = (10,1)), sg.Input('', key = '_FNAME_')],
+    [sg.Text('Last name: ', size = (10,1)), sg.Input('', key = '_LNAME_')],
+    [sg.Text('Telephone: ', size = (10,1)), sg.Input('', key = '_TELEPHONE_')],
+    [sg.Text('E-mail: ', size = (10,1)), sg.Input('', key = '_EMAIL_')],
+    [sg.Text('')],
+    [sg.Text('', size = (39,1)), sg.Button('Save', size=(10, 1))]
+]
+
+layout = [
+    [sg.Column(layout=layout1), sg.Column(layout=layout2)]
+
+]
+
+window = sg.Window(title = 'Phonebook', layout=layout)
+
+while True:
+    event, values = window.read()
+
+    if event == None:
+        break
+
+window.close()
